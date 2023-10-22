0b3d15cac7bc3b7f398b6bc15002b91c0f659808
Joyce Lee <joyce.lee.chocolate@gmail.com>
2023-10-22 18:53:42 -0400

    added basic functions to handle data submition to backend

--- src/components/AddTripForm.jsx
+++ src/components/AddTripForm.jsx
@@ -2,17 +2,51 @@ import React, {useState} from 'react'
 import { addItinerary, addTrip } from '../api/authApi'
 
 export default function AddTripForm() {
+  // variables needed for state management
   const [tripName, setTripName] = useState('');
   const [travelDates, setTravelDates] = useState('');
   const [location, setLocation] = useState('');
   const [duration, setDuration] = useState('');
   const [itinerary, setItinerary] = useState('');
+// function for handling adding state changes to backend
+  const handleTripSubmit = async(event) => {
+    event.preventDefault();
+    // handling trip name separately from rest of form as independent variable, might need to change this to match your backend variables
+    const newTripNameData = {
+      tripName, 
+    }
+    // calling addTrip with new trip
+
+    const res = await addTrip(newTripNameData);
+    // checking to see if it works
+    console.log('Trip added:', res)
+  }
+// function to handle the rest of the form data
+  const handleItinerarySubmit = async(event) => {
+    event.preventDefault();
+
+    // setting variables for form data, might need to change this to match your backend variables
+    const newItineraryData = {
+      travelDates, 
+      location, 
+      duration, 
+      itinerary
+    }
+
+    // calling addItinerary with new itinerary data;
+    const res = await addItinerary(newItineraryDatabase);
+
+    // checking to see if it is passing data
+
+    console.log('Itinerary Added:', res)
+  }
   return (
     <div className='add__trip__container'>
         <form className='add__trip__form'>
             <div>
               <label htmlFor="">Trip Name:
-              <input type="text"/>
+              <input 
+              type="text"/>
               </label>
             </div>
 
