1b90fe4677352b9bad4db78b77f5389541dee069
Joyce Lee <joyce.lee.chocolate@gmail.com>
2023-10-25 00:21:54 -0400

    working on api

--- src/components/EditTripForm.jsx
+++ src/components/EditTripForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import { getAllItinerary, updateTrip, updateItinerary } from '../api/authApi'
 import UserContext from '../contexts/UserContext';
 import { useParams } from 'react-router-dom';
--- src/pages/Itinerary.jsx
+++ src/pages/Itinerary.jsx
@@ -3,14 +3,17 @@ import UserContext from '../contexts/UserContext';
 import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import './itinerary.css';
+import UpdateTripForm from '../components/EditTripForm';
+import TripList from '../components/ComponentTrips';
+
 
 
 
 export default function Itinerary(){
 
     const [itinerary, setItinerary] = useState({});
+    const [isFormVisible, setIsFormVisible] = useState(false);
     const userToken = useContext(UserContext);
-
     const params = useParams();
 
     useEffect(() => {
@@ -29,24 +32,33 @@ export default function Itinerary(){
         }
     }, [userToken, params.tripId]);
 
+    const handleEditTripClick = () => {
+        setIsFormVisible(!isFormVisible);
+      };
+
 return(
     <>
     <h2>My Itinerary</h2>
     {itinerary.length > 0 ? (
     <div className='itinerary__list__container'>
         {/* <p>Trip Number: {itinerary.id} </p> */}
-        <p>Location: {itinerary[0].travel_location}</p>
+        <p>Trip Name: {itinerary[0].trip_id.trip_name}</p>
         <p>Travel Date Starts: {itinerary[0].travel_date_begin}</p>
         <p>Travel Date Ends: {itinerary[0].travel_date_end}</p>
         <p>Travel Duration: {itinerary[0].travel_duration} Days</p>
+        <p>Location: {itinerary[0].travel_location}</p>
         <p>Detail Itinerary: {itinerary[0].detail_itinerary}</p>
     </div>
     ) : (
         <p> Loading ... </p>
     )}
     <div> 
-    <button className='edit__button'>Edit</button>
+    <button className='edit__button' onClick={handleEditTripClick}>Edit</button>
     </div>
+    {/* <div>
+        {isFormVisible && <UpdateTripForm/>}
+    </div> */}
+    <div id="map">map</div>
     </>
 );
 
